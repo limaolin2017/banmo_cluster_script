a2f525644ce5d606e70ef69c41114d833071d02d
Maolin Li <limaolin2013@gmail.com>
2023-10-11 00:51:54 +0200

    could be rungit add banmo.shgit add banmo.sh

--- banmo.sh
+++ banmo.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #SBATCH -J 3D_reconstruction      # name
-#SBATCH -p high              	     # Queue name
-#SBATCH -N 2                      		     # 2 node
-#SBATCH --gres=gpu:2              	     # GPUs per node
+#SBATCH -p high                      # Queue name
+#SBATCH -N 2                   # total number of nodes requested (16 cores/node)
+#SBATCH --gres=gpu:2                         # GPUs per node
+#SBATCH --mem=16g                     # Memory per node.
 #SBATCH --exclude=node[001-018],node[031-032]  # Exclude the nodes with faulty GPUs
 #SBATCH --time=99:99:99                # maximum execution time (HH:MM:SS)
 #SBATCH -o slurm_3D.%N.%J.out       # Name of the standard output file
@@ -25,7 +26,7 @@ seqname="cat-pikachiu"
 addr="10001"
 use_human="no"
 use_symm="no"
-pose_cnn_path="./banmo/mesh_material/posenet/quad.pth"
+pose_cnn_path="./mesh_material/posenet/quad.pth"
 num_epochs=120
 batch_size=256
 model_prefix="3D_reconstruction_cat_pikachiu"
@@ -39,5 +40,3 @@ bash scripts/template-mgpu.sh $gpus $savename \
  --lineload --batch_size $batch_size\
  --${use_symm}symm_shape \
  --${use_human}use_human
-
-
